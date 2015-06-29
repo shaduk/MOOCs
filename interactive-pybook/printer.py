0a099cb0bc54736b1426396f8946398d1d6e249a
Shad Ullah Khan <shaduk@gmail.com>
2015-06-29 14:46:51 +0000

    Added class Task and implemented printer

--- interactive-pybook/printer.py
+++ interactive-pybook/printer.py
@@ -46,3 +46,51 @@ class Printer:
         self.currentTask = newtask
         self.timeRemaining = newtask.getPages()*60/self.pagerate
         
+
+class Task:
+    
+    def __init__(self,time):
+        self.timestamp = time
+        self.pages = random.randrange(1,21)
+        
+    def getStamp(self):
+        return self.timestamp
+        
+    def getPages(self):
+        return self.pages
+        
+    def waitTime(self,currenttime):
+        return currenttime - self.timestamp
+
+
+def simulation(numSeconds, pagesPerMinute):
+
+    labprinter = Printer(pagesPerMinute)
+    printQueue = Queue()
+    waitingtimes = []
+
+    for currentSecond in range(numSeconds):
+
+      if newPrintTask():
+         task = Task(currentSecond)
+         printQueue.enqueue(task)
+
+      if (not labprinter.busy()) and (not printQueue.isEmpty()):
+        nexttask = printQueue.dequeue()
+        waitingtimes.append(nexttask.waitTime(currentSecond))
+        labprinter.startNext(nexttask)
+
+      labprinter.tick()
+
+    averageWait=sum(waitingtimes)/len(waitingtimes)
+    print("Average Wait %6.2f secs %3d tasks remaining."%(averageWait,printQueue.size()))
+
+def newPrintTask():
+    num = random.randrange(1,181)
+    if num == 180:
+        return True
+    else:
+        return False
+
+for i in range(10):
+    simulation(3600,6)
