f368e19068eed274a616c1c6f48932e9d5002feb
Shad Ullah Khan <shaduk@gmail.com>
2015-07-01 13:30:50 +0000

    List implementation

--- interactive-pybook/listimplement.py
+++ interactive-pybook/listimplement.py
@@ -17,5 +17,57 @@ class Node:
         self.next = newnext
         
     
-temp = Node(23)
-print temp.getData()
+class UnorderedList:
+    
+    def __init__(self):
+        self.head = None
+        
+    def add(self,item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+    
+    def size(self):
+        count = 0
+        current = self.head 
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+            
+        return count
+        
+    def search(self,item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+        
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData()==item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+        
+
+q = UnorderedList()
+
+q.add(12)
+q.add(11)
+q.add(3)
+q.add(2)
+q.add(76)
+print q.size()
