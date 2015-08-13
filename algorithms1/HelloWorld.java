1f69334c452b5d93b07fb0ffb9c756812058de1a
Shad Ullah Khan <shaduk@gmail.com>
2015-08-13 13:37:46 +0000

    added code for whodunit

--- algorithms1/HelloWorld.java
+++ algorithms1/HelloWorld.java
@@ -1,41 +1,8 @@
-import java.util.Scanner;
-
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        
-        System.out.println("Enter the string");
-        Scanner scan= new Scanner(System.in);
-        String x = scan.nextLine();
-
-        for(int i=0;i<x.length();i++)
-        {
-            char c= x.charAt(i);
-     int ASCII =(int) c ;
-     if(ASCII>96 && ASCII<123)
-     
-    { 
-        ASCII= ((ASCII-97+3)%26)+97;
-        
-    }
-    else if(ASCII>64 && ASCII<91)
+public class HelloWorld
+{
+    public static void main(String[] args)
     {
-        ASCII= ((ASCII-65+3)%26)+65;
+        System.out.print("Hello, World");
+        System.out.println();
     }
-     
-        
-    
-     else 
-     {
-         
-        System.out.println(x) ;
-     }
-        char b = (char) ASCII;
-        System.out.print(b);
-         
-        }
-    
-
-            
-    }  
 }
