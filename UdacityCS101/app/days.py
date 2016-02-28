fd7887aa637916fa70ec90230e7a1b81731ff3ca
Shad Ullah Khan <shaduk@gmail.com>
2016-02-28 18:27:19 +0000

    added past code from udacity cs101

---
+++ UdacityCS101/app/days.py
@@ -0,0 +1,36 @@
+def nextDay(year, month, day):
+    """Simple version: assume every month has 30 days"""
+    if day < 30:
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
+        
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar, and the first date is not after
+       the second."""
+    c=0
+    while(year1,month1,day1 != year2,month2,day2):
+        print year1,month1,day1
+        year1,month1,day1=nextDay(year1,month1,day1)
+        c = c+1
+    # YOUR CODE HERE!
+    return c
+
+def test():
+    test_cases = [((2012,9,30,2012,10,30),30), 
+                  ((2012,1,1,2013,1,1),360),
+                  ((2012,9,1,2012,9,4),3)]
+    
+    for (args, answer) in test_cases:
+        result = daysBetweenDates(*args)
+        if result != answer:
+            print "Test with data:", args, "failed"
+        else:
+            print "Test case passed!"
+
+test()
